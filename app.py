10994bf8fa138e405caf1c3f9553f289a72b3476
prrao87 <prrao87@gmail.com>
2019-08-23 17:32:32 -0400

    Add secret key and empty string error handler

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, request, render_template
 from lime_explainer import explainer, tokenizer, METHODS
 
 app = Flask(__name__)
-
+SECRET_KEY = os.urandom(24)
 
 @app.route('/')
 def index():
@@ -13,6 +14,8 @@ def index():
 def explain():
     if request.method == 'POST':
         text = tokenizer(request.form['entry'])
+        if not text:
+            raise ValueError("Please enter a sentence with at least a few words.")
         method = request.form['classifier']
         exp = explainer(method, path_to_file=METHODS[method]['file'], text=text)
         exp = exp.as_html()
@@ -21,4 +24,5 @@ def explain():
 
 
 if __name__ == '__main__':
+    app.secret_key = SECRET_KEY
     app.run(debug=True)
